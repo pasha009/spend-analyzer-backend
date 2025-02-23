b0f1312b5941b6a4a5c4d2815574affbcadefa7f
Shrutesh Patil <shruteshpatil009@gmail.com>
2025-02-23 10:54:11 +0530

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+const app = express()
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Expense Manager API")
+});
+
+const PORT = process.env.PORT ?? 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
